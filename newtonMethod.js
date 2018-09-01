df49beb65db4d837133150c7e5566f520d830a5d
Kumar-Achuth <achuthkumar146@gmail.com>
2018-09-01 16:44:15 +0530

    commit

---
+++ newtonMethod.js
@@ -0,0 +1,9 @@
+var prompt = require('prompt-sync')()
+var utility= require('/home/bridgeit/Documents/kumar/algorithmPrograms/utility/utility.js')
+ function newtonsMethod()
+ {
+     var c=prompt('Enter the non-negative number for c : ')
+     utility.newtonsMethod(c)
+
+ }
+ newtonsMethod();
